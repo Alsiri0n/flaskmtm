e5ac5975d1892d9b07062d0ad9cf7f014d207d95
Ignatev Nikita <nikitaignatiev@gmail.com>
2022-12-09 20:54:42 +0300

    Add test for create_app

--- tests/test_flaskmtm.py
+++ tests/test_flaskmtm.py
@@ -1,5 +1,23 @@
-from src.flaskmtm import __version__
+import pytest
+from flaskmtm import create_app
 
 
-def test_version():
-    assert __version__ == "0.1.0"
+@pytest.fixture()
+def app():
+    app = create_app("testing")
+    yield app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
+
+
+def test_request_index():
+    response = client.get("/")
+    assert 200 in response.data in response.status_code
